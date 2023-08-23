676eaab4fdc034d54c876be6c5125048a08a87aa
Lucassouzateixeira <lucassouzateixeira2@gmail.com>
2023-08-23 08:51:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+// Protocolo HTTP.
+const http = require("http")
+
+// Endereço da maquina que será o servidor.
+const host = "localhost"
+
+// A Porta é um número que da acesso ao servidor, ela pode ir de 1 até 65235.
+const porta = 3000
+
+const resposta = function(req, res){
+    res.end(`
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <style>
+                body{
+                    background-image: linear-gradient(to right, white, blue)
+                }
+                
+                .container{
+                  text-align: center
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Meu nome é Lucas Souza Teixeira, tenho 18 anos e estou cursando Tecnico em Informatica no IFMS.</h1>
+        </body>
+    </html>
+    `)
+}
+
+const servidor = http.createServer(resposta)
+
+// Um servidor, nada mais é do que um "Programa" que tem como finalidade escutar uma porta e dar uma resposta. 
+servidor.listen(porta, host, function(){
+    console.log("Servidor funcionando.")
+})
